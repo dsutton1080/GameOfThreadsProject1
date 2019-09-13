62dcddadf4cfc8d652a58a31810153118e4181b0
DeRon Sutton <dsutton1080@gmail.com>
2019-09-13 15:53:49 -0500

    Added a function to choose who is player 1

--- battleship/Setup and Close/setup.py
+++ battleship/Setup and Close/setup.py
@@ -1,8 +1,25 @@
 # import tkinter as tk
+import random
 
 
 def startup():
     print("Welcome to Battleship!")
+    plays_first()
+
+
+def plays_first():
+    coin_choice = input("Lets begin, heads or tails?: ")
+    coin_list = ["heads", "tails"]
+    correct = random.choice(coin_list)
+    if (coin_choice.lower() != "heads") & (coin_choice.lower() != "tails"):
+        print("Well, since you were unable to enter heads or tails I will choose for you.")
+        print("Person to the left is Player1 and person to the right is Player2.")
+    elif coin_choice.lower() == correct:
+        print("If you chose " + correct + " you are Player1")
+        print("The other person is Player2")
+    elif coin_choice.lower() != correct:
+        print("If you chose " + correct + " you are Player2")
+        print("The other person is Player1")
 
 
 def get_num_of_ships():
