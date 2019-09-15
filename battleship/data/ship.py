1fcc8ba073063a27f286035459578166d01d4cf5
WCStarks <49533865+WCStarks@users.noreply.github.com>
2019-09-15 13:24:53 -0500

    Update ship.py
    Edited hit() method and added comments

--- battleship/data/ship.py
+++ battleship/data/ship.py
@@ -1,29 +1,41 @@
 # A ship is represented by a list of lists
 # The inner list contains a 2-tuple representing a coordinate and
-#    boolean value indicating whether it has been hit there
+#    boolean value indicating whether it has been hit at that coordinate
 
 
 class Ship:
     def __init__(self, size, coords):
-        self.size = size
-        self.coords = coords
-        self.isSunk = False
-        self.numHits = 0
+        self.size = size        # Size of the ship
+        self.coords = coords    # List of tuples of coordinates and boolean values
+        self.isSunk = False     # Indicates whether the ship has been sunk
+        self.numHits = 0        # Number of time the ship has been hit
 
-    def hit(self, position):
-        for coord in self.coords:
+    # Returns the index in self.coords of a position if the ship occupies that position
+    #   Otherwise returns -1
+    def getCoordIndex(self, position):
+        for index, coord in enumerate(self.coords):
             if position == coord[0]:
-                coord[1] = True
-                print(f"You hit {coord[0]}!\n")
+                return index
+        return -1
+
+    # Changes the boolean value associated with a coordinate to True
+    #   If this hit would sink the ship, changes isSunk value to True
+    #   Returns True on a success and False if the ship does not occupy the given position
+    def hit(self, position):
+        index = self.getCoordIndex(position)
+        success = False
+        if index >= 0:
+            if position == self.coords[index][0]:
+                self.coords[index][1] = True
+                success = True
                 self.numHits += 1
-                break
-        if self.numHits == self.size:
-            self.isSunk = True
-            print(f"You sunk the {self.size} ship!\n")
+            if self.numHits == self.size:
+                self.isSunk = True
+        return success
 
 
 # Returns a Ship object given a start coordinate and end coordinate
-#   represented by 2-Tuples - (row, column)
+#   The start and end parameters should be 2-tuples given as (row, column)
 def createShip(start, end):
     coords = []
     size = 0
@@ -36,14 +48,5 @@ def createShip(start, end):
         for i in range(start[0], end[0] + 1):
             coords.append([(i, start[1]), False])
     newShip = Ship(size, coords)
-    print(newShip.coords)
     return newShip
 
-
-# All code below this is for testing
-myShip = createShip((0, 0), (0, 4))
-
-for x in range(0,5):
-    myShip.hit((0,x))
-
-print(myShip.coords)
