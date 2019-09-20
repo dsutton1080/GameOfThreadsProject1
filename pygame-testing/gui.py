aeca22ff1c2242bebfbc7b0593b746a47238cf64
Jace Kline <jace_kline@ku.edu>
2019-09-20 18:14:32 -0500

    Added many functions to handle the orientation of the ships on the board. Attempted to get multi-square highlighting, but still in progress.

--- pygame-testing/gui.py
+++ pygame-testing/gui.py
@@ -75,17 +75,18 @@ class Board:
         x, y = self.window_location
         squares = []
         offsetY = 0
+        row = 1
         for rowY in [(y + (self.squareHeight * i)) for i in range(1, 9)]:
-            row = 1
             offsetX = 0
+            col = 1
             for colX in [(x + (self.squareWidth * i)) for i in range(1, 9)]:
-                col = 1
                 squares = squares + [
                     BoardSquare((row, col), (colX + offsetX, rowY + offsetY), self.squareWidth, self.squareHeight)]
                 col += 1
                 offsetX += self.offset
             row += 1
             offsetY += self.offset
+
         return squares
 
     def create_row_labels(self):
@@ -153,7 +154,7 @@ class Ship(pygame.sprite.Sprite):
         self.offset = SCREEN_HEIGHT / 400
         self.surface = pygame.Surface((self.squareWidth, self.squareHeight * self.length))
         self.rect = self.surface.fill(self.color).move(self.window_coord[0], self.window_coord[1])
-    
+
 
 def highlight(obj, color):
     obj.surface.fill(color)
@@ -169,6 +170,72 @@ def quit_actions():
 
 def is_quit_case(event):
     return event == pygame.QUIT
+
+
+def next_orientation(currentOrientation):
+    return (currentOrientation + 1) % 4
+
+
+def orientation_to_ship_end_coord(anchor, shipLength, orientation):
+    row, col = anchor
+    mapping = {
+        0: (row + shipLength, col),
+        1: (row, col + shipLength),
+        2: (row - shipLength, col),
+        3: (row, col - shipLength)
+    }
+    return mapping[orientation]
+
+
+def is_on_board(coord):
+    x, y = coord
+    if (x > 0) and (x < 9) and (y > 0) and (y < 9):
+        return True
+    return False
+
+
+def is_coord_conflict(coordList1, coordList2):
+    for coord in coordList1:
+        if coord in coordList2:
+            return True
+    return False
+
+
+def is_possible_orientation(anchor, shipLength, orientation, otherFilledCoords):
+    return (is_on_board(orientation_to_ship_end_coord(anchor, shipLength, orientation))) and (
+        not is_coord_conflict(orientation_to_coord_list(anchor, shipLength, orientation), otherFilledCoords))
+
+
+def orientation_to_coord_list(anchor, shipLength, orientation):
+    def abs_range(a, b):
+        if a > b:
+            return range(b, a)
+        else:
+            return range(a, b)
+
+    row, col = anchor
+    endRow, endCol = orientation_to_ship_end_coord(anchor, shipLength, orientation)
+
+    rowRange = abs_range(row, endRow)
+    colRange = abs_range(col, endCol)
+
+    if not rowRange:
+        return map(lambda c: (row, c), colRange)
+    else:
+        return map(lambda r: (r, col), rowRange)
+
+
+# returns an orientation code 0-3, otherwise returns None if not possible
+def first_possible_orientation(anchor, shipLength, otherFilledCoords):
+    for i in [0, 1, 2, 3]:
+        if is_possible_orientation(anchor, shipLength, i, otherFilledCoords):
+            return i
+    return None
+
+
+def coord_to_board_square(board):
+    print(board.squares)
+    return lambda coord: (filter(lambda s: s.grid_coord == coord, board.squares))[0]
 
 
 # background = pygame.image.load('battleship.jpg').convert()
@@ -243,6 +310,8 @@ def get_intersect_object_from_list(pos, ls):
             return obj
     return None
 
+
+
 # def draw_and_update(obj):
 
 
@@ -298,7 +367,24 @@ def run_place_ships(numShips):
     def get_hovered_square(pos):
         return get_intersect_object_from_list(pos, placeBoard.squares)
 
-    def run_choose_board_location(ship):
+    def run_choose_board_location(ship, otherShipCoords):
+
+        # highlight the updateBoard squares that correspond to each coordinate in the passed in list of coordinates
+        def highlight_suggestion_placement(coordList, color):
+            print("Hello", list(coordList))
+
+            highlight(placeBoard)
+            # targetSquares = map(lambda c: coord_to_board_square(placeBoard)(c), coordList)
+            # l = []
+            # for coord in coordList:
+            #     for square in placeBoard.squares:
+            #         if coord == square.grid_coord:
+            #             l += coord
+            #             highlight(square, color)
+            # print(l)
+            # for square in targetSquares:
+            #     highlight(square, color)
+            #     pygame.display.update(square.rect)
 
         def wait_for_click(square):
             while True:
@@ -309,7 +395,7 @@ def run_place_ships(numShips):
                         return False
 
 
-        def run_rotate_ship(shipLength, anchorCoord):
+        def run_rotate_ship(shipLength, anchorCoord, firstOrientation):
             instructionsTextBoxRotate = TextBox("Use the UP and DOWN arrow keys to rotate your ship.", (96, 10), fontsize=36)
             instructionsTextBoxEnter = TextBox("Press ENTER when you are satisfied with the orientation.", (96, 56), fontsize=36)
             instructionsTextBoxEscape = TextBox("Press the ESC button to cancel placing this ship.", (96, 102), fontsize=36)
@@ -339,20 +425,25 @@ def run_place_ships(numShips):
                 elif event.type == MOUSEMOTION:
                     hoveredSquare = get_hovered_square(event.pos)
                     if not hoveredSquare == None:
-
-                        ## Change from highlighting only the anchor square to highlighting a valid ship orientation ##
-
-                        highlight(hoveredSquare, colors['GREEN'])
-                        didClick = wait_for_click(hoveredSquare)
-                        if didClick:
-                            shipCoords = run_rotate_ship(ship.length, hoveredSquare.grid_coord)
-                            if not shipCoords == None:
-                                return shipCoords
-                        else:
-                            highlight(hoveredSquare, colors['GREY'])
+                        firstOrientation = first_possible_orientation(hoveredSquare.grid_coord, ship.length, otherShipCoords)
+                        if not firstOrientation == None:
+                            print(firstOrientation)
+                            print(hoveredSquare.grid_coord)
+                            print(ship.length)
+                            highlightCoords = orientation_to_coord_list(hoveredSquare.grid_coord, ship.length, firstOrientation)
+                            print(list(highlightCoords))
+                            highlight_suggestion_placement(list(highlightCoords), colors['GREEN'])
+                            didClick = wait_for_click(hoveredSquare)
+                            if didClick:
+                                shipCoords = run_rotate_ship(ship.length, hoveredSquare.grid_coord, firstOrientation)
+                                if not shipCoords == None:
+                                    return shipCoords
+                            else:
+                                highlight_suggestion_placement(orientation_to_coord_list(hoveredSquare.grid_coord, ship.length, firstOrientation), colors['GREY'])
+            pygame.time.delay(100)
 
     # event loop
-    shipList = []
+    shipCoordsList = []
 
     while True:
         for event in pygame.event.get():
@@ -366,11 +457,11 @@ def run_place_ships(numShips):
                     highlight(clickedShip, colors['GREEN'])
                     # screen.blit(clickedShip.surface, clickedShip.rect)
                     # pygame.display.update(clickedShip.rect)
-                    chosenLocation = run_choose_board_location(clickedShip)
+                    chosenLocation = run_choose_board_location(clickedShip, shipCoordsList)
                     if chosenLocation == None:
                         highlight(clickedShip, colors['BLUE'])
                     else:
-                        shipList += [chosenLocation]
+                        shipCoordsList += [chosenLocation]
                         shipQueue.remove(clickedShip)
                         blit_objects(screen, shipQueue)
 
